596435103808d449dc572e19e40f626f37a82d66
Beesigaa <adejohadamah@gmail.com>
2024-09-30 04:38:57 -0700

    basic authentication

--- 0x01-Basic_authentication/8-all.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Defines list_all function"""
-
-
-def list_all(mongo_collection):
-    """lists all documents in a collection"""
-    return mongo_collection.find()
--- 0x01-Basic_authentication/9-insert_school.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Defines insert_school function"""
-
-
-def insert_school(mongo_collection, **kwargs):
-    """Inserts a new document in a collection based on kwargs"""
-    inserted_obj = mongo_collection.insert_one(kwargs)
-
-    return inserted_obj.inserted_id
